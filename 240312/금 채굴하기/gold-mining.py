0fedf81f2d39e0552154e3c4a196a1248331d714
gsbang97 <qkdrldbs12@gmail.com>
2024-03-12 00:15:33 +0900

    Title: 금 채굴하기, Time: 136ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240312/금 채굴하기/gold-mining.py
@@ -0,0 +1,25 @@
+n, cost = map(int, input().split())
+gold_mat = [list(map(int, input().split())) for _ in range(n)]
+max_gold = 0
+for i in range(n):
+    for j in range(n):
+        for k in range(n):
+            k_cost = k*k+(k+1)*(k+1)
+            gold = 0
+            for l in range(k):
+                j_1 = j - k - l
+                j_2 = j + k - l
+                if j_1 < 0:
+                    j_1 = 0
+                if j_2 >= n :
+                    j_2 = n-1
+                if l == 0:
+                    gold += sum(gold_mat[i][j_1:j_2])
+                else:
+                    if i+l < n:
+                        gold += sum(gold_mat[i+l][j_1:j_2])
+                    if i-l >= 0 :
+                        gold += sum(gold_mat[i-l][j_1:j_2])
+            if gold*cost >= k_cost:
+                max_gold = max(max_gold, gold)
+print(gold)
