5ea97850591971a5b844fad68980873aacf28730
gsbang97 <qkdrldbs12@gmail.com>
2024-03-11 20:29:15 +0900

    Title: 오목, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240311/오목/O-mok.py
+++ 240311/오목/O-mok.py
@@ -3,23 +3,24 @@ result = 0
 for i in range(19):
     for j in range(19):
         if board[i][j] != 0:
-            if i < 14:
-                check = board[i][j] 
+            check = board[i][j] 
+            if i < 15:
                 if check == board[i+1][j] and check == board[i+2][j] and check == board[i+3][j] and check == board[i+4][j]:
                     print(check)
                     print(i+3, j+1)
                     exit()
-            if j < 14:
+            if j < 15:
+                # print( check == board[i][j+1] and check == board[i][j+2] and check == board[i][j+3] and check == board[i][j+4])
                 if check == board[i][j+1] and check == board[i][j+2] and check == board[i][j+3] and check == board[i][j+4]:
                     print(check)
                     print(i+1, j+3)
                     exit()
-            if i < 14 and j < 14:
+            if i < 15 and j < 15:
                 if check == board[i+1][j+1] and check == board[i+2][j+2] and check == board[i+3][j+3] and check == board[i+4][j+4]:
                     print(check)
                     print(i+3, j+3)
                     exit()
-            if i > 3 and j < 14:
+            if i > 3 and j < 15:
                 if check == board[i-1][j+1] and check == board[i-2][j+2] and check == board[i-3][j+3] and check == board[i-4][j+4]:
                     print(check)
                     print(i-1, j+3)
