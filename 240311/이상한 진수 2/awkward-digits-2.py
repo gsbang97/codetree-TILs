510604a7b608178ae94bc9423bb38095b5a94aa7
gsbang97 <qkdrldbs12@gmail.com>
2024-03-11 18:48:41 +0900

    Title: 이상한 진수 2, Time: 147ms, Memory: 24MB, Status: Passed - CodeTree

--- 240311/이상한 진수 2/awkward-digits-2.py
+++ 240311/이상한 진수 2/awkward-digits-2.py
@@ -3,8 +3,10 @@ idx = -1
 sum = 0
 flag = True
 for i, s in enumerate(a):
-    sum += 2**(len(a)-i-1)
+    if s == '1':
+        sum += 2**(len(a)-i-1)
     if s == '0' and flag:
+        sum += 2**(len(a)-i-1)
         flag = False
 
 if flag:
