e3a77b8c179ad419dfb5df94d1b55523d8139616
gsbang97 <qkdrldbs12@gmail.com>
2024-03-11 20:25:55 +0900

    Title: 오목, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240311/오목/O-mok.py
+++ 240311/오목/O-mok.py
@@ -19,4 +19,9 @@ for i in range(19):
                     print(check)
                     print(i+3, j+3)
                     exit()
+            if i > 3 and j < 14:
+                if check == board[i-1][j+1] and check == board[i-2][j+2] and check == board[i-3][j+3] and check == board[i-4][j+4]:
+                    print(check)
+                    print(i-1, j+3)
+                    exit()
 print(0)
