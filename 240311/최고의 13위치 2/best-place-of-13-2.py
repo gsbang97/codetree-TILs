4b2cece9a1d68d28eddfe91751a41d73dd50ea0b
gsbang97 <qkdrldbs12@gmail.com>
2024-03-11 20:47:14 +0900

    Title: 최고의 13위치 2, Time: 175ms, Memory: 26MB, Status: Passed - CodeTree

--- 240311/최고의 13위치 2/best-place-of-13-2.py
+++ 240311/최고의 13위치 2/best-place-of-13-2.py
@@ -6,16 +6,19 @@ for i in range(N):
         first = sum(mat[i][j:j+3])
         for k in range(i, N):
             if k == i:
-                for l in range(j+1,N-2):
+                for l in range(j+3,N-2):
                     second = sum(mat[k][l:l+3])
                     s = first + second
-                    # print(i,j,k,l)
+                    # if s > 2:
+                    #     print(i,j,k,l)
                     max_num = max(s, max_num)
             else:
                 for l in range(N-2):
                     second = sum(mat[k][l:l+3])
-                    # print(i,j,k,l)
+                    
                     s = first + second
+                    # if s > 2:
+                    #     print(i,j,k,l)
                     max_num = max(s, max_num)
 
 print(max_num)
