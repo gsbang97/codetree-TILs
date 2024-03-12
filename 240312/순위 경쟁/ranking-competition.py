b66bbe35a5bc8248f421fddbe2c04e8650529ab8
gsbang97 <qkdrldbs12@gmail.com>
2024-03-12 17:07:14 +0900

    Title: 순위 경쟁, Time: 155ms, Memory: 24MB, Status: Passed - CodeTree

--- 240312/순위 경쟁/ranking-competition.py
+++ 240312/순위 경쟁/ranking-competition.py
@@ -1,7 +1,7 @@
 #  점수 변동 관련 정보가 주어졌을 때, 명예의 전당에 올라간 사람의 조합이 총 몇 번 바뀌었는지를 출력
 n = int(input())
 scores = [0,0,0]
-rank = 5
+rank = 7
 next_rank = 0
 cnt = 0
 for _ in range(n):
@@ -19,6 +19,7 @@ for _ in range(n):
         if max_score == scores[i]:
             next_rank += 2**i
     if next_rank != rank:
+
         rank = next_rank
         cnt +=1
 print(cnt)
