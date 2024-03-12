684dc2d83ba70257cd363a7b24b14488609c73d5
gsbang97 <qkdrldbs12@gmail.com>
2024-03-12 23:55:40 +0900

    Title: 세 수의 합, Time: 318ms, Memory: 52MB, Status: Passed - CodeTree

--- 240312/세 수의 합/sum-of-three-num.py
+++ 240312/세 수의 합/sum-of-three-num.py
@@ -1,39 +1,18 @@
 # n : 정수의 개수 , k : 세 수를 더 했을 때 나와야하는 값
 n,k = map(int, input().split())
 seq = {}
+seq2 = {}
+cnt = 0
 for i in map(int, input().split()):
+    if k-i in seq2.keys():
+        cnt += seq2[k-i]
+    for key in seq.keys():
+        if key+i in seq2.keys():
+            seq2[key + i] += seq[key]
+        else:
+            seq2[key + i] = seq[key]
     if i in seq.keys():
         seq[i] += 1
     else:
         seq[i] = 1
-keys = list(seq.keys())
-cnt = 0
-visited = set()
-for i in range(len(keys)):
-    for j in range(i, len(keys)):
-        s = keys[i]+keys[j]
-        if k - s in keys:
-            if i == j:
-                if keys[j] == (k - s):
-                    cnt += (seq[keys[i]]*(seq[keys[i]]-1)*(seq[keys[i]]-2)) // 6
-                    visited.add(keys[i])
-                elif k - s not in visited:
-                    cnt += (seq[keys[i]]*(seq[keys[i]]-1)//2) * seq[k - s]
-                    visited.add(keys[i])
-                    visited.add((k-s))
-            elif k - s not in visited:
-                if keys[i] == (k - s):
-                    visited.add(keys[i])
-                    visited.add(keys[j])
-                    cnt += (seq[keys[i]]*(seq[keys[i]]-1)//2) * seq[keys[j]]
-                elif keys[j] == (k - s):
-                    visited.add(keys[i])
-                    visited.add(keys[j])
-                    cnt += (seq[keys[j]]*(seq[keys[j]]-1)//2) * seq[keys[i]]
-                elif (k-s):
-                    cnt += seq[keys[i]]*seq[keys[j]]*seq[k-s]
-                    visited.add(keys[i])
-                    visited.add(keys[j])
-            # print(keys[i],keys[j],k-s,cnt)
-                
 print(cnt)
