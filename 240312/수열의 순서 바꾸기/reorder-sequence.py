9c06bcc1fd6ed138b9c2e8eb80b24ecf3c79cae6
gsbang97 <qkdrldbs12@gmail.com>
2024-03-12 18:52:36 +0900

    Title: 수열의 순서 바꾸기, Time: 141ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240312/수열의 순서 바꾸기/reorder-sequence.py
+++ 240312/수열의 순서 바꾸기/reorder-sequence.py
@@ -4,6 +4,8 @@ sorted_arr = sorted(arr)
 cnt = 0
 for i in range(n-1,-1,-1):
     if sorted_arr[i] != arr[i]:
-        cnt = i
+        for j in range(i):
+            if sorted_arr[i] == arr[j]:
+                cnt = j+1
         break
 print(cnt)
