ea9e347cbafbeec2a91d39d8bcee5785f81ddc7b
gsbang97 <qkdrldbs12@gmail.com>
2024-03-12 23:04:37 +0900

    Title: 가장 많은 데이터, Time: 246ms, Memory: 34MB, Status: Passed - CodeTree

--- 240312/가장 많은 데이터/most-frequent-data.py
+++ 240312/가장 많은 데이터/most-frequent-data.py
@@ -6,5 +6,8 @@ for _ in range(n):
         seq[s] += 1
     else:
         seq[s] = 1
+max_item = 0
+for k,v in seq.items():
+    max_item = max(max_item,v)
 
-print(max(seq.items())[1])
+print(max_item)
