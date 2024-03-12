309b9ad51c63ac325a7b3ae5dc9c980dc0eef91b
gsbang97 <qkdrldbs12@gmail.com>
2024-03-12 13:44:32 +0900

    Title: 소괄호 매칭 수 최대화하기, Time: 583ms, Memory: 39MB, Status: Passed - CodeTree

--- 240312/소괄호 매칭 수 최대화하기/maximize-the-number-of-parenthesis-matches.py
+++ 240312/소괄호 매칭 수 최대화하기/maximize-the-number-of-parenthesis-matches.py
@@ -3,29 +3,19 @@ from functools import cmp_to_key
 def score(s):
     l = len(s)
     cnt = 0
+    lefts = 0
     for i in range(l):
         if s[i] == '(':
-            for j in range(i+1, l):
-                if s[j] == ')':
-                    cnt += 1
+            lefts += 1
+        else:
+            cnt += lefts
     return cnt
-def compare(x,y):
-    s1 = score(x+y)
-    s2 = score(y+x)
-    if s1 > s2 :
-        return -1
-    elif s1 < s2 :
-        return 1
-    else:
-        return 0
-n = int(input())
-# strs = [input() for _ in range(n)]
-# strs.sort(key=cmp_to_key(compare))
+
 def score_2(s):
     left = s.count('(')
     right = s.count(')')
     return (left,right, score(s))
-def compare2(x,y):
+def compare(x,y):
     s1 = x[0]*y[1] + x[2] + y[2]
     s2 = x[1]*y[0] + x[2] + y[2]
     if s1 > s2 :
@@ -34,22 +24,15 @@ def compare2(x,y):
         return 1
     else:
         return 0
+n = int(input())
 strs = [score_2(input()) for _ in range(n)]
-# print(strs)
-strs.sort(key=cmp_to_key(compare2))
-# print(strs)
+strs.sort(key=cmp_to_key(compare))
 scores = strs[0][2]
 lefts = strs[0][0]
 for i in range(1,n):
     
     rights = strs[i][1]
     scores += lefts*rights + strs[i][2]
-    # print(lefts, rights, scores)
     lefts += strs[i][0]
-    # for j in range(i+1):
-    #     lefts += strs[j][0]
-    # for j in range(i+1,n):
-    #     rights += strs[j][1]
-    # print(strs[i][2], lefts, rights)
     
 print(scores)
