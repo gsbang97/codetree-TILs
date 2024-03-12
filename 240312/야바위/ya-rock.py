25ff2c18376c122cb7e6dd1082f521536a4c6aef
gsbang97 <qkdrldbs12@gmail.com>
2024-03-12 12:45:04 +0900

    Title: 야바위, Time: 125ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240312/야바위/ya-rock.py
@@ -0,0 +1,15 @@
+N = int(input())
+# a번 종이컵과 b번 종이컵을 교환한 뒤, c번 종이컵을 열어 그 안에 조약돌이 있으면 점수를 얻는 행동
+behaviors = [tuple(map(int, input().split())) for _ in range(N)]
+max_score = 0
+for i in range(1,3):
+    stone = [False]+[i==j for j in range(3)]
+    score = 0
+    for a,b,c in behaviors:
+        tmp = stone[a]
+        stone[a] = stone[b]
+        stone[b] = tmp
+        if stone[c]:
+            score +=1
+    max_score = max(score, max_score)
+print(max_score)
