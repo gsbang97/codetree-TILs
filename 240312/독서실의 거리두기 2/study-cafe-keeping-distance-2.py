6540a010c7713e41ee77188c789705a0482db643
gsbang97 <qkdrldbs12@gmail.com>
2024-03-12 15:02:31 +0900

    Title: 독서실의 거리두기 2, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

--- 240312/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
+++ 240312/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
@@ -5,15 +5,17 @@ seat = input()
 min_diff = 1000
 max_diff = 0
 start = -1
+first_diff = 0
 # end = -1
 for i,s in enumerate(seat):
     if s == '1':
         if start == -1:
             start = i
+            first_diff = i
         else:
             dist = i - start
             max_diff = max(max_diff, dist)
             min_diff = min(min_diff, dist)
             start = i
-max_diff = max(max_diff//2, N-start-1)
+max_diff = max(max_diff//2, N-start-1, first_diff)
 print(min(min_diff, max_diff))
