24ef6e7d82a4fe9d1ba041bdc551ba9b7f365f3c
gsbang97 <qkdrldbs12@gmail.com>
2024-03-12 13:35:10 +0900

    Title: 소괄호 매칭 수 최대화하기, Time: 5055ms, Memory: 34MB, Status: Time Limit Exceed - CodeTree

--- 240312/소괄호 매칭 수 최대화하기/maximize-the-number-of-parenthesis-matches.py
+++ 240312/소괄호 매칭 수 최대화하기/maximize-the-number-of-parenthesis-matches.py
@@ -19,8 +19,33 @@ def compare(x,y):
     else:
         return 0
 n = int(input())
-strs = [input() for _ in range(n)]
-
-strs.sort(key=cmp_to_key(compare))
-s = ''.join(strs)
-print(score(s))
+# strs = [input() for _ in range(n)]
+# strs.sort(key=cmp_to_key(compare))
+def score_2(s):
+    left = s.count('(')
+    right = s.count(')')
+    return (left,right, score(s))
+def compare2(x,y):
+    s1 = x[0]*y[1] + x[2] + y[2]
+    s2 = x[1]*y[0] + x[2] + y[2]
+    if s1 > s2 :
+        return -1
+    elif s1 < s2 :
+        return 1
+    else:
+        return 0
+strs = [score_2(input()) for _ in range(n)]
+# print(strs)
+strs.sort(key=cmp_to_key(compare2))
+# print(strs)
+scores = 0
+for i in range(n):
+    lefts = strs[i][0]
+    rights = 0
+    # for j in range(i+1):
+    #     lefts += strs[j][0]
+    for j in range(i+1,n):
+        rights += strs[j][1]
+    # print(strs[i][2], lefts, rights)
+    scores += strs[i][2] + lefts*rights
+print(scores)
