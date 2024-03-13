c5ebe389862082235939c7b4590a17d8c1604c1d
gsbang97 <qkdrldbs12@gmail.com>
2024-03-13 19:40:24 +0900

    Title: 겹치지 않게 선분 고르기, Time: 127ms, Memory: 26MB, Status: Passed - CodeTree

--- 240313/겹치지 않게 선분 고르기/select-segments-without-overlap.py
+++ 240313/겹치지 않게 선분 고르기/select-segments-without-overlap.py
@@ -3,7 +3,7 @@ n = int(input())
 lines = [tuple(map(int, input().split())) for _ in range(n)]
 
 able = [0 for _ in range(1001)]
-
+line_used = [True for _ in range(n)]
 def line_check(x,y):
     for i in range(x,y+1):
         if able[i] == 1:
@@ -11,19 +11,23 @@ def line_check(x,y):
     return True
     
 max_cnt = 0
-def choose(cnt):
+def choose(cnt, line_idx):
     can_cnt = 0
-    for line in lines:
-        x,y = line
-        if line_check(x,y):
-            for i in range(x,y+1):
-                able[i] = 1
-            choose(cnt+1)
-            for i in range(x,y+1):
-                able[i] = 0
-            can_cnt += 1
+    for idx in range(line_idx, n):
+        line = lines[idx]
+        if line_used[idx]:
+            line_used[idx] = False
+            x,y = line
+            if line_check(x,y):
+                for i in range(x,y+1):
+                    able[i] = 1
+                choose(cnt+1,idx)
+                for i in range(x,y+1):
+                    able[i] = 0
+                can_cnt += 1
+            line_used[idx] = True
     if can_cnt == 0:
         global max_cnt
         max_cnt = max(max_cnt, cnt)
-choose(0)
+choose(0,0)
 print(max_cnt)
