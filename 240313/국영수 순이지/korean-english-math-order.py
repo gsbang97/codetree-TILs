ccbf38b9e667179ebf4806b21805413f919f9554
gsbang97 <qkdrldbs12@gmail.com>
2024-03-13 16:25:23 +0900

    Title: 국영수 순이지, Time: 134ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240313/국영수 순이지/korean-english-math-order.py
@@ -0,0 +1,6 @@
+n = int(input())
+students = [ input().split() for _ in range(n) ]
+students.sort(key = lambda x : (-int(x[1]),-int(x[2]),-int(x[3])))
+
+for student in students:
+    print(*student)
