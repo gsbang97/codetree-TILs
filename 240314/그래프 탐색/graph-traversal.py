8e6a0d99c1db8941801b5e22e8f0022ec0d151e3
gsbang97 <qkdrldbs12@gmail.com>
2024-03-14 20:43:45 +0900

    Title: 그래프 탐색, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240314/그래프 탐색/graph-traversal.py
@@ -0,0 +1,25 @@
+# dfs
+# N개의 정점과 M개의 간선으로 이루어진 양방향 그래프
+# 1번 정점에서 시작하여 주어진 간선을 따라 이동했을 때 도달 할 수 있는 서로 다른 정점의 수
+
+n,m = map(int, input().split())
+grape = [[False for _ in range(n+1)] for _ in range(n+1)]
+for _ in range(m):
+    x,y = map(int, input().split())
+
+    grape[x][y] = True
+    grape[y][x] = True
+
+visited = [False for _ in range(n+1)]
+visited[1] = True
+cnt = 0
+def dfs(current):
+    global cnt
+    for x in range(n):
+        if grape[current][x] and not visited[x]:
+            visited[x] = True
+            cnt += 1
+            dfs(x)
+
+dfs(1)
+print(cnt)
