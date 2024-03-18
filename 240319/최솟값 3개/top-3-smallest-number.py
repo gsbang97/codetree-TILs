5ad303651ae7a4613f77407d0a4dbd679254125f
gsbang97 <qkdrldbs12@gmail.com>
2024-03-19 02:35:04 +0900

    Title: 최솟값 3개, Time: 188ms, Memory: 29MB, Status: Wrong Answer - CodeTree

---
+++ 240319/최솟값 3개/top-3-smallest-number.py
@@ -0,0 +1,24 @@
+from heapq import heappop, heappush
+
+n = int(input())
+
+numbers = list(map(int, input().split()))
+min_numbers = []
+max_number = 0
+# max_numbers = []
+ans = 1
+for i, number in enumerate(numbers):
+    if i < 3:
+        heappush(min_numbers, -number)
+        ans *= number
+        if i < 2:
+            print(-1)
+        else:
+            max_number = -heappop(min_numbers)
+            print(ans)
+    else:
+        if max_number > number:
+            ans = ans * number // max_number
+            heappush(min_numbers, -number)
+            max_number = heappop(min_numbers)
+        print(ans)
