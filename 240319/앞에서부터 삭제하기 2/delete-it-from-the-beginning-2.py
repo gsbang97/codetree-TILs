2674d679a51cf9b86cb653dc480c6380f26bfcd3
gsbang97 <qkdrldbs12@gmail.com>
2024-03-19 00:40:22 +0900

    Title: 앞에서부터 삭제하기 2, Time: 249ms, Memory: 35MB, Status: Passed - CodeTree

--- 240319/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
+++ 240319/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
@@ -5,9 +5,13 @@ n = int(input())
 numbers = deque(map(int, input().split()))
 
 sum_number = sum(numbers)
+min_number = min(numbers)
 que = []
 for k in range(1,n-2):
-    sum_number -= numbers.popleft()
-    avr = (sum_number - min(numbers)) / (n - k - 1)
+    value = numbers.popleft()
+    sum_number -= value
+    if value == min_number:
+        min_number = min(numbers)
+    avr = (sum_number - min_number) / (n - k - 1)
     heapq.heappush(que,-avr)
 print(f"{-heapq.heappop(que):.2f}")
