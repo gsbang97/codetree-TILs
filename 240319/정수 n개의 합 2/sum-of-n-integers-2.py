0061bffd8122645bd5dc425337c21fe3ce48eaf7
gsbang97 <qkdrldbs12@gmail.com>
2024-03-19 21:17:42 +0900

    Title: 정수 n개의 합 2, Time: 259ms, Memory: 31MB, Status: Passed - CodeTree

--- 240319/정수 n개의 합 2/sum-of-n-integers-2.py
+++ 240319/정수 n개의 합 2/sum-of-n-integers-2.py
@@ -5,10 +5,10 @@ n,k = map(int, input().split())
 numbers = list(map(int, input().split()))
 sum_numbers = [0 for _ in range(n+1)]
 max_num = 0
-for i in range(1,n+1-k):
+for i in range(1,n+1):
     sum_numbers[i] = sum_numbers[i-1]+numbers[i-1]
     if i >= k :
         number = sum_numbers[i] - sum_numbers[i-k]
         max_num = max(max_num, number)
-
+# print(sum_numbers)
 print(max_num)
