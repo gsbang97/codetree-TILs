bc0210c2b126ed537d0bcfcaf7be75e21676694c
gsbang97 <qkdrldbs12@gmail.com>
2024-03-20 22:48:08 +0900

    Title: 폭탄 해체 작업, Time: 269ms, Memory: 76MB, Status: Passed - CodeTree

--- 240320/폭탄 해체 작업/the-bomb-dismantling.py
+++ 240320/폭탄 해체 작업/the-bomb-dismantling.py
@@ -5,14 +5,16 @@ bombs = []
 
 for _ in range(n):
     point, time = map(int, input().split())
-    heapq.heappush(bombs,(time, -point))
+    heapq.heappush(bombs,(-point, time))
 
-current_time = 0
+can_time = [True for _ in range(10001)]
 score = 0
 while bombs:
-     time, point = heapq.heappop(bombs)
-     if current_time < time :
-        score += -point
-        current_time += 1
+     point,time = heapq.heappop(bombs)
+     for t in range(time-1, -1, -1):
+        if can_time[t]:
+            can_time[t] = False
+            score += -point
+            break
 
 print(score)
