3e86d2d988ddb2e383e390948ef1d993aef7e0f0
gsbang97 <qkdrldbs12@gmail.com>
2024-03-20 23:38:19 +0900

    Title: 적석과 흑석 2, Time: 5076ms, Memory: 50MB, Status: Time Limit Exceed - CodeTree

--- 240320/적석과 흑석 2/red-stone-and-black-stone-2.py
+++ 240320/적석과 흑석 2/red-stone-and-black-stone-2.py
@@ -5,14 +5,18 @@ c,n = map(int, input().split())
 reds = set([int(input()) for _ in range(c)])
 blacks = [tuple(map(int, input().split())) for _ in range(n)]
 # reds.sort()
-blacks.sort(key=lambda x: (x[1]-x[0]))
+blacks.sort(key=lambda x: (x[1]-x[0],x[1]))
 cnt = 0
 # red_idx = 0
 black_idx = 0
+# max_red = 1000000000
 for A,B in blacks:
-    for t in range(A,B+1):
-        if t in reds:
+    # for t in range(B,A-1,-1):
+    for t in sorted(reds):
+        if A <= t and t <= B:
             reds.remove(t)
             cnt += 1
+            break
+        elif t  > B :
             break
 print(cnt)
