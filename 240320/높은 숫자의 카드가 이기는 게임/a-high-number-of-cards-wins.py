28166c688ae83720f525f7536fc834b8c33e8725
gsbang97 <qkdrldbs12@gmail.com>
2024-03-20 22:16:34 +0900

    Title: 높은 숫자의 카드가 이기는 게임, Time: 3563ms, Memory: 36MB, Status: Time Limit Exceed - CodeTree

--- 240320/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.py
+++ 240320/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.py
@@ -2,20 +2,24 @@ n = int(input())
 
 B_cards = [int(input()) for _ in range(n)]
 A_cards = set(range(1,2*n+1)) - set(B_cards)
+A_cards = [True for _ in range(2*n+1)]
+A_cards[0] = False
+for card in B_cards:
+    A_cards[card] = False
 
 score = 0
 for card in B_cards:
     is_win = False
     for card in range(card+1,2*n+1):
-        if card in A_cards:
+        if A_cards[card]:
             is_win = True
-            A_cards.remove(card)
+            A_cards[card] = False
             break
     if is_win:
         score += 1
         continue
     for card in range(card):
-        if card in A_cards:
-            A_cards.remove(card)
+        if A_cards[card]:
+            A_cards[card] = False
             break
 print(score)
