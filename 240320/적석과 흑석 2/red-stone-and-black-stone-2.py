07af8c4b091f6220a90a64053175c84c7fcb4c44
gsbang97 <qkdrldbs12@gmail.com>
2024-03-20 23:13:47 +0900

    Title: 적석과 흑석 2, Time: 378ms, Memory: 31MB, Status: Wrong Answer - CodeTree

---
+++ 240320/적석과 흑석 2/red-stone-and-black-stone-2.py
@@ -0,0 +1,23 @@
+# 1부터 C까지 번호가 매겨진 빨간 돌 C개
+# 1부터 N 까지 번호가 매겨진 검정 돌 N개
+c,n = map(int, input().split())
+
+reds = [int(input()) for _ in range(c)]
+blacks = [tuple(map(int, input().split())) for _ in range(n)]
+reds.sort()
+blacks.sort()
+cnt = 0
+red_idx = 0
+black_idx = 0
+while black_idx < n and red_idx < c:
+    t = reds[red_idx]
+    A,B = blacks[black_idx]
+    if A <= t and t <= B:
+        red_idx += 1
+        black_idx += 1
+        cnt += 1
+    elif t < A:
+        red_idx += 1
+    else:
+        black_idx +=1
+print(cnt)
