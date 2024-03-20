c2e81449af86dd7fac2e2d5b4650657e12535be8
gsbang97 <qkdrldbs12@gmail.com>
2024-03-20 22:21:24 +0900

    Title: 높은 숫자의 카드가 이기는 게임, Time: 3565ms, Memory: 36MB, Status: Time Limit Exceed - CodeTree

--- 240320/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.py
+++ 240320/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.py
@@ -8,18 +8,20 @@ for card in B_cards:
     A_cards[card] = False
 
 score = 0
+min_card = 0
 for card in B_cards:
     is_win = False
-    for card in range(card+1,2*n+1):
-        if A_cards[card]:
+    for win_card in range(card+1,2*n+1):
+        if A_cards[win_card]:
             is_win = True
-            A_cards[card] = False
+            A_cards[win_card] = False
             break
     if is_win:
         score += 1
         continue
-    for card in range(card):
-        if A_cards[card]:
-            A_cards[card] = False
+    for lose_card in range(min_card,card):
+        if A_cards[lose_card]:
+            min_card = lose_card
+            A_cards[lose_card] = False
             break
 print(score)
