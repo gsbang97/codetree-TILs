dc70b34e94de788aa829f2fc68c8219eebe0e8a9
gsbang97 <qkdrldbs12@gmail.com>
2024-03-21 13:35:49 +0900

    Title: 소괄호 매칭 수 최대화하기, Time: 599ms, Memory: 39MB, Status: Passed - CodeTree

--- 240321/소괄호 매칭 수 최대화하기/maximize-the-number-of-parenthesis-matches.py
+++ 240321/소괄호 매칭 수 최대화하기/maximize-the-number-of-parenthesis-matches.py
@@ -1,5 +1,16 @@
-import heapq
+from functools import cmp_to_key
 
+def compare(x,y):
+    x_left, x_right, x_score = x
+    y_left, y_right, y_score = y
+    xy = x_left*y_right
+    yx = y_left*x_right
+    if xy > yx:
+        return -1
+    elif xy == yx:
+        return 0
+    else:
+        return 1
 n = int(input())
 strings = []
 for _ in range(n):
@@ -13,15 +24,14 @@ for _ in range(n):
         if char == ")":
             right += 1
             score += left
-    heapq.heappush(strings, (-left,right,score))
+    strings.append((left,right,score))
+strings.sort(key=cmp_to_key(compare))
 lefts = 0
-# rights = 0 
 ans = 0
-for _ in range(n):
-    left, right, score = heapq.heappop(strings)
+for i in range(n):
+    left, right, score = strings[i]
     if right > 0:
-        # rights += right
         ans += lefts * right
-    lefts -= left
+    lefts += left
     ans += score
 print(ans)
