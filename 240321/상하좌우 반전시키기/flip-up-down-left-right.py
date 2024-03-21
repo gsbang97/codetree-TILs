35ed85b7ddb8eac7655a10928600b3afbea82900
gsbang97 <qkdrldbs12@gmail.com>
2024-03-21 15:33:00 +0900

    Title: 상하좌우 반전시키기, Time: 206ms, Memory: 26MB, Status: Passed - CodeTree

--- 240321/상하좌우 반전시키기/flip-up-down-left-right.py
+++ 240321/상하좌우 반전시키기/flip-up-down-left-right.py
@@ -1,72 +1,29 @@
 import sys
 n = int(input())
+# sys.setrecursionlimit(max(1000,n**3)) 
 numbers = [list(map(int, input().split())) for _ in range(n)]
 
 if n == 1:
     print(0 if numbers[0][0] == 1 else -1)
     sys.exit()
-
-min_push = n*n
-dxs,dys = [0,1,0,-1],[1,0,-1,0]
-def next_choose(x,y, push_cnt):
-    if y == n-1:
-        choose(x+1,0, push_cnt)
-    else:
-        choose(x,y+1, push_cnt)
-
-def is_push(x,y):
-    if x > 0:
-        if numbers[x-1][y] == 1:
-            return False
-    if x == n-1 and y > 0:
-        if numbers[x][y-1] == 1:
-            return False
-    return True
-def is_not_push(x,y):
-    if x > 0:
-        if numbers[x-1][y] == 0:
-            return False
-    if x == n-1 and y > 0:
-        if numbers[x][y-1] == 0:
-            return False
-    return True
 def is_range(x,y):
     return x < n and x >= 0 and y < n and y >= 0
 def push(x,y):
     numbers[x][y] = 1 if numbers[x][y] == 0 else 0
+    dxs, dys = [0,0,1],[1,-1,0]
     for dx,dy in zip(dxs,dys):
         nx, ny = x + dx, y + dy
         if is_range(nx,ny):
             numbers[nx][ny] = 1 if numbers[nx][ny] == 0 else 0
-push_cnt_tmp = 0
-final = 0
-def choose(x,y, push_cnt):
-    global push_cnt_tmp, final
-    global min_push
-    # if x == 1:
-    #     print(x,y)
-    # if push_cnt_tmp < push_cnt and final == 15:
-    #     push_cnt_tmp = push_cnt
-    #     print(x,y, push_cnt, min_push)
-    #     for i in range(n):
-    #         print(*numbers[i])
-    
-    if x == n-1 and y == n-1:
-        push_cnt_tmp = 0
-        final += 1
-        if numbers[x][y] == 1 and numbers[x-1][y] == 1 and numbers[x][y-1] == 1:
-            min_push = min(min_push, push_cnt)
-        elif numbers[x][y] == 0 and numbers[x-1][y] == 0 and numbers[x][y-1] == 0:
-            min_push = min(min_push, push_cnt + 1)
-        return
-    if is_push(x,y):
-        push(x,y)
-        next_choose(x,y, push_cnt+1)
-        push(x,y)
-    if is_not_push(x,y):
-        next_choose(x,y, push_cnt)
-choose(0,1,0)
-if min_push == n*n:
-    min_push = -1
-print(min_push)
-# print(final)
+answer = 0
+for i in range(1,n):
+    for j in range(n):
+        if i == n-1 and j > 0:
+           if numbers[i-1][j] != numbers[i][j-1] :
+            answer = -1
+            break
+        if numbers[i-1][j] == 0:
+            answer += 1  
+            push(i,j)
+        
+print(answer)
