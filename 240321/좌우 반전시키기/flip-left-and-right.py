778f7549eefc5c371010bf5322a9b4ce75663769
gsbang97 <qkdrldbs12@gmail.com>
2024-03-21 14:15:44 +0900

    Title: 좌우 반전시키기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240321/좌우 반전시키기/flip-left-and-right.py
+++ 240321/좌우 반전시키기/flip-left-and-right.py
@@ -3,6 +3,8 @@ n = int(input())
 numbers = list(map(int, input().split()))
 min_push = 101
 def choose(cnt, push):
+    # print(cnt,push)
+    # print(numbers)
     global min_push
     if cnt == n-1:
         if numbers[-1] == 1 and numbers[-2] == 1:
@@ -13,10 +15,6 @@ def choose(cnt, push):
     
     if cnt == 0:
         choose(cnt + 1, push)
-        numbers[cnt] = 0 if numbers[cnt] == 1 else 1
-        numbers[cnt+1] = 0 if numbers[cnt+1] == 1 else 1
-        
-        choose(cnt + 1, push + 1)    
     elif cnt < n-1:
         if numbers[cnt-1] == 1:
             choose(cnt + 1, push)
