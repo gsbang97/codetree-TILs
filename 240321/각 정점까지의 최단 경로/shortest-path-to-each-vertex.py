025667d2aaa06cd60b9393189c9dabb7c49cc414
gsbang97 <qkdrldbs12@gmail.com>
2024-03-21 17:03:26 +0900

    Title: 각 정점까지의 최단 경로, Time: 945ms, Memory: 49MB, Status: Passed - CodeTree

--- 240321/각 정점까지의 최단 경로/shortest-path-to-each-vertex.py
+++ 240321/각 정점까지의 최단 경로/shortest-path-to-each-vertex.py
@@ -16,18 +16,19 @@ heapq.heappush(que, (0,k))
 dist = [sys.maxsize for _ in range(n+1)]
 dist[k] = 0
 while que:
-    value, idx = heapq.heappop(que)
+    min_dist, min_idx = heapq.heappop(que)
     # try:
-    if value != dist[idx]:
+    if min_dist != dist[min_idx]:
         continue
     # except:
     #     print(idx, " error")
     #     continue
     
-    for num, d in grape[idx]:
-        if dist[num] > (dist[idx] + d) :
-            dist[num] = dist[idx] + d
-            heapq.heappush(que, (dist[num],d))
+    for idx, d in grape[min_idx]:
+        new_dist = dist[min_idx] + d
+        if dist[idx] > new_dist :
+            dist[idx] = new_dist
+            heapq.heappush(que, (new_dist,idx))
 
 for i in range(1,n+1):
     if dist[i] == sys.maxsize:
