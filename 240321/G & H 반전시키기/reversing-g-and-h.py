4dc49f172f036a984e593f2eddd434a587088855
gsbang97 <qkdrldbs12@gmail.com>
2024-03-21 13:53:05 +0900

    Title: G & H 반전시키기, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240321/G & H 반전시키기/reversing-g-and-h.py
@@ -0,0 +1,22 @@
+n = int(input())
+
+
+A = input()
+B = input()
+not_flip = 0
+flip = 0
+prev_same = False
+is_init = True
+for a,b in zip(A,B):
+    if a == b:
+        if not prev_same or is_init:
+            is_init = False
+            flip += 1
+        prev_same = True
+    else:
+        if prev_same or is_init:
+            is_init = False
+            not_flip += 1
+        prev_same = False
+ans = min(flip, not_flip+1)
+print(ans)
