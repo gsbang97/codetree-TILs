78a9cc27de3be7dcd36965838c339d438b2506a4
gsbang97 <qkdrldbs12@gmail.com>
2024-03-21 15:51:02 +0900

    Title: 0과 1로 이루어진 격자, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

--- 240321/0과 1로 이루어진 격자/grid-consisting-of-0-and-1.py
+++ 240321/0과 1로 이루어진 격자/grid-consisting-of-0-and-1.py
@@ -2,14 +2,23 @@ n = int(input())
 
 numbers = [input() for _ in range(n)]
 
+row_flip = [False for _ in range(10)]
+col_flip = [False for _ in range(10)]
 push = 0
 is_flip = False
 for i in range(n-1,-1,-1):
     for j in range(n-1,-1,-1):
+        is_flip = col_flip[j]
         if numbers[i][j] == '0' and is_flip:
             push += 1
-            is_flip = False if is_flip else True
+            # for x in range(i+1):
+            #     row_flip[x] = False if row_flip[x] else True
+            for y in range(j+1):
+                col_flip[y] = False if col_flip[y] else True
         if numbers[i][j] == '1' and not is_flip:
             push += 1
-            is_flip = False if is_flip else True
+            # for x in range(i+1):
+            #     row_flip[x] = False if row_flip[x] else True
+            for y in range(j+1):
+                col_flip[y] = False if col_flip[y] else True
 print(push)
